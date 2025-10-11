dc3c685151395c73cd32291a334336ac8e0ee0ad
Evgenii Onishchenko <e.onishchenko@start.ru>
2025-10-11 18:01:39 +0300

    add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header.jsx';
 import Hero from './components/Hero_new.jsx';
 import Services from './components/Services.jsx';
@@ -8,10 +10,11 @@ import Team from './components/Team.jsx';
 import FAQ from './components/FAQ.jsx';
 import ConsultationForm from './components/ConsultationForm.jsx';
 import Footer from './components/Footer.jsx';
-import ChatWidget from './components/ChatWidget.jsx';
-import AdminLogin from './admin/AdminLogin.jsx';
-import AdminDashboard from './admin/AdminDashboard.jsx';
-import ProtectedRoute from './admin/ProtectedRoute.jsx';
+
+const ChatWidget = lazy(() => import('./components/ChatWidget.jsx'));
+const AdminLogin = lazy(() => import('./admin/AdminLogin.jsx'));
+const AdminDashboard = lazy(() => import('./admin/AdminDashboard.jsx'));
+const ProtectedRoute = lazy(() => import('./admin/ProtectedRoute.jsx'));
 
 function App() {
     return (
@@ -30,18 +33,27 @@ function App() {
                             <FAQ />
                             <ConsultationForm />
                             <Footer />
-                            <ChatWidget />
+
+                            <Suspense>
+                                <ChatWidget />
+                            </Suspense>
                         </>
                     } />
 
                     {/* Admin Routes */}
-                    <Route path="/admin" element={<AdminLogin />} />
+                    <Route path="/admin" element={
+                        <Suspense fallback={<div className='flex items-center justify-center'>Загружается</div>}>
+                                <AdminLogin />
+                        </Suspense>
+                    } />
                     <Route 
                         path="/admin/dashboard" 
                         element={
-                            <ProtectedRoute>
-                                <AdminDashboard />
-                            </ProtectedRoute>
+                            <Suspense fallback={<div className='flex items-center justify-center'>Загружается</div>}>
+                                <ProtectedRoute>
+                                    <AdminDashboard />
+                                </ProtectedRoute>
+                            </Suspense>
                         } 
                     />
                 </Routes>
