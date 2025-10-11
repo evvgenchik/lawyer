6b93c9a3622bda7f015c3ac7a34378ca7390ef16
Evgenii Onishchenko <e.onishchenko@start.ru>
2025-10-11 20:04:41 +0300

    updates to deploy on github

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const ProtectedRoute = lazy(() => import('./admin/ProtectedRoute.jsx'));
 
 function App() {
     return (
-        <Router>
+        <Router basename="/lawyer">
             <div className="App">
                 <Routes>
                     {/* Public Routes */}
